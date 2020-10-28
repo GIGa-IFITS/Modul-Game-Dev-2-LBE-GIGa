4e33b31babbf2768f3e7d048565649e6032de184
Geizka Wahyu Fahriza <geizka.fahriza@gmail.com>
2020-10-28 22:07:05 +0700

    added project

---
+++ project/Assets/Scripts/HUDCoin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDCoin : MonoBehaviour {
+
+	private Player player;
+	private Text text;
+
+	void Start() {
+		text = GetComponent<Text>();
+		player = FindObjectOfType<Player>();
+	}
+	
+	void Update() {
+		text.text = "Koin : " + player.Coin;
+	}
+}
---
+++ project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		Player player = other.GetComponent<Player>();
+		if(player != null) {
+			GameOverMenu gameOverMenu = FindObjectOfType<GameOverMenu>();
+			gameOverMenu.SetScore(player.Coin);
+			gameOverMenu.Show();
+			Destroy(player.gameObject);
+		}
+	}
+
+}
